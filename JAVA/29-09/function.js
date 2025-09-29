797099c4f25280369e0bcc405625a508add1bd41
gitanshugautam <gitanshugautam82@gmail.com>
2025-09-29 12:36:24 +0530

    add file

---
+++ JAVA/29-09/function.js
@@ -0,0 +1,22 @@
+const greet = function(){
+    console.log("hello world")
+}
+greet()
+
+
+
+// const greet = () =>{
+//     console.log("hello world")
+
+// }
+// greet()
+
+// function functionDeclaration()
+
+// const functionExpression = function(){
+//     console.log("hello Function Expression")
+
+// }
+// functionExpression()
+// const arrowFunction()
+// const arrowFunction = ()=>
